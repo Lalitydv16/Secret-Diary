21e503097ebc2c49da0d6a8f2715bfefd4be615a
Lalitydv16 <89172450+Lalitydv16@users.noreply.github.com>
2021-08-27 10:15:49 +0530

    Update my_users.sql

--- my_users.sql
+++ my_users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2021 at 01:45 PM
+-- Generation Time: 27 , aug, 2021 at 01:45 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.33
 
